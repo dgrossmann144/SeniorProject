3366b9bfc2d35b6f96bcca2c17362dc0bde04ce0
Danny G <dgrossmann@gmail.com>
2017-04-10 15:28:43 -0400

    Wrote population class

--- src/Population.java
+++ src/Population.java
@@ -1,5 +1,34 @@
 
 public class Population
 {
-
+	private Individual individuals[];
+	
+	public Population(int size, boolean initialize)
+	{
+		individuals = new Individual[size];
+		if(initialize)
+		{
+			for(int x = 0; x < size; x++)
+			{
+				Individual newIndividual = new Individual();
+				newIndividual.generate();
+				saveIndividual(x, newIndividual);
+			}
+		}
+	}
+	
+	public Individual getIndividual(int x)
+	{
+		return individuals[x];
+	}
+	
+	public int size()
+	{
+		return individuals.length;
+	}
+	
+	public void saveIndividual(int x, Individual individual)
+	{
+		individuals[x] = individual;
+	}
 }
