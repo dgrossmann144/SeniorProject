e5f2d0da3e5c691a516095ee033b309a1c80e82d
Danny G <dgrossmann@gmail.com>
2017-04-10 12:48:12 -0400

    Created driver;

---
+++ src/Driver.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Driver extends JPanel
+{
+	public static void main(String[] args) 
+	{
+		JFrame frame = new JFrame ("Santa Fe Ant Problem");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
