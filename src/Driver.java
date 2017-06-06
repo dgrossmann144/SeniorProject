d2699aa68bae6048edbf9b089c4884f3957c47f2
Daniel Grossmann <117DGrossmann@frhsd.com>
2017-06-06 10:04:37 -0400

    comments

--- src/Driver.java
+++ src/Driver.java
@@ -21,24 +21,35 @@ public class Driver extends JPanel implements KeyListener
 	private BufferedImage greenPath = null;
 	private BufferedImage yoshi = null;
 	private static Point pos = new Point(0, 0);
+	/**
+	 * Stores the initial board for resetting
+	 */
 	private static int[][] gridInput = new int[32][32];
+	/**
+	 * User interface
+	 */
 	private static Driver panel;
-	private static int speed;
-	private static int numFruitLeft; // number of fruit left on the map
-	private static int totalFruit;
+	private static int speed = 1;
+	private static int numFruitLeft;
+	private static int totalFruit = 0;
+	/**
+	 * Index of population
+	 */
 	private static int popNum = 1;
+	/**
+	 * Current population
+	 */
 	private static Population pop;
-	private static boolean readGrid = false;
 	/**
 	 * 0 = path<br>
 	 * 1 = fruit<br>
 	 * 2 = completed path<br>
-s	 */
+	 */
 	private static int[][] grid = new int[32][32];
 	
 	public static void main(String[] args) 
 	{
-		speed = 1;
+		//Initializes window for drawing to
 		JFrame frame = new JFrame ("Santa Fe Ant Problem");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		panel = new Driver();
@@ -47,46 +58,45 @@ s	 */
 		frame.setResizable(false);
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
-		pop = new Population(100, true);
 		
-		double fitness = tick();
-		while(numFruitLeft != 0 && fitness < .5)
+		pop = new Population(100, true);
+		double fitness = calcFitness();
+		readGrid();
+		
+		//Main loop 
+		while(numFruitLeft != 0 || fitness < 89)
 		{
-			fitness = tick();
+			fitness = calcFitness();
 			System.out.println("Achieved a fitness of " + fitness);
 		}
+		frame.dispose();
 	}
 	
 	public Driver()
 	{
 		addKeyListener(this);
-		this.setPreferredSize(new Dimension(1008, 1008));
+		this.setPreferredSize(new Dimension(1014, 1014));
 		setFocusable(true);
 		requestFocus();
 	}
 	
-	private static double tick()
+	private static double calcFitness()
 	{
+		//Loops through individuals
 		indivLoop:for(int indiv = 0; indiv < pop.size(); indiv++)
 		{
-			//Reset grid between individuals
+			//Resets the grid between individuals
 			numFruitLeft = totalFruit;
-			if(!readGrid)
+			for(int x = 0; x < grid.length; x++)
 			{
-				readGrid();
-				readGrid = true;
-			} else
-			{
-				for(int x = 0; x < grid.length; x++)
+				for(int y = 0; y < grid[x].length; y++)
 				{
-					for(int y = 0; y < grid[x].length; y++)
-					{
-						grid[x][y] = gridInput[x][y];
-					}
+					grid[x][y] = gridInput[x][y];
 				}
 			}
 			pos.setLocation(0, 0);
 			
+			//Checks first space for fruit
 			if (grid[pos.x][pos.y] == 1)
 			{
 				numFruitLeft--;
@@ -96,6 +106,7 @@ s	 */
 			
 			for(int gene = 0; gene < pop.getIndividual(indiv).size(); gene++)
 			{
+				//Controls movement based on gene
 				switch(pop.getIndividual(indiv).getGene(gene))
 				{
 					case 0:
@@ -114,25 +125,30 @@ s	 */
 				
 				if(bound())
 				{
+					//Stops current individual
 					pop.getIndividual(indiv).spaces = pop.getIndividual(indiv).size();
 					continue indivLoop;
 				} else
 				{
+					//Collects a fruit if pos is on a fruit
 					if (grid[pos.x][pos.y] == 1)
 					{
 						numFruitLeft--;
 						pop.getIndividual(indiv).apples++;
 					}
+					//Stops if it retraces path
 					if(grid[pos.x][pos.y] == 2)
 					{
 						pop.getIndividual(indiv).spaces = pop.getIndividual(indiv).size();
 						continue indivLoop;
 					}
+					//Stops if all fruit are collected
 					if(numFruitLeft == 0)
 					{
 						pop.getIndividual(indiv).spaces++;
 						continue indivLoop;
-					} else
+					}
+					else //Empty space
 					{
 						grid[pos.x][pos.y] = 2;
 						pop.getIndividual(indiv).spaces++;
@@ -140,27 +156,23 @@ s	 */
 				}
 			}
 		}
-		return reset();
+		double fitness = pop.getIndividual(pop.getFittest()).getFitness();
+		reset();
+		return fitness;
 	}
 	
-	private static double reset()
+	private static void reset()
 	{
+		//Displays information about the current population
 		System.out.println("Population " + popNum + " complete, max fitness: " + pop.getIndividual(pop.getFittest()).getFitness());
 		System.out.println(pop.getIndividual(pop.getFittest()));
 		System.out.println();
-		popNum++;
-		pos.setLocation(0, 0);
-		for(int x = 0; x < grid.length; x++)
-		{
-			for(int y = 0; y < grid[x].length; y++)
-			{
-				grid[x][y] = gridInput[x][y];
-			}
-		}
 		showFittest(pop.getIndividual(pop.getFittest()).genes);
+		
+		//Evolves next population
 		Population newPop = Algorithims.evolve(pop);
 		pop = newPop;
-		return pop.getIndividual(pop.getFittest()).getFitness();
+		popNum++;
 	}
 	
 	protected void paintComponent(Graphics g)
@@ -176,6 +188,7 @@ s	 */
 			e.printStackTrace();
 		}
 		
+		//Prints grid
 		for(int x = 0; x < 32; x++)
 		{
 			for(int y = 0; y < 32; y++)
@@ -199,6 +212,10 @@ s	 */
 		g.drawImage(yoshi, (int)pos.getX() * 32, (int)pos.getY() * 32, 32, 32, null);
 	}
 	
+	/**
+	 * Reads input grid to store fruit locations
+	 * (Called once)
+	 */
 	private static void readGrid()
 	{
 		try
@@ -215,11 +232,11 @@ s	 */
 					gridInput[y][x] = grid[y][x];
 					if(Integer.parseInt(input[y]) == 1)
 					{
-						numFruitLeft++;
+						totalFruit++;
 					}
 				}
 			}
-			totalFruit = numFruitLeft;
+			numFruitLeft = totalFruit;
 			scan.close();
 		} catch (FileNotFoundException e)
 		{
@@ -227,6 +244,9 @@ s	 */
 		}
 	}
 
+	/**
+	 * Used to control simulation speed
+	 */
 	public void keyPressed(KeyEvent key)
 	{
 		if(key.getKeyCode() == KeyEvent.VK_EQUALS && speed < Integer.MAX_VALUE/2)
@@ -249,6 +269,9 @@ s	 */
 		
 	}
 	
+	/**
+	 * @return Whether the position is in the grid
+	 */
 	private static boolean bound()
 	{
 		if(pos.getX() < 0)
@@ -268,10 +291,23 @@ s	 */
 		return false;
 	}
 	
+	/**
+	 * Displays the current best path
+	 * @param genes Current best path of instructions
+	 */
 	private static void showFittest(int[] genes)
 	{
+		for(int x = 0; x < grid.length; x++)
+		{
+			for(int y = 0; y < grid[x].length; y++)
+			{
+				grid[x][y] = gridInput[x][y];
+			}
+		}
+		pos.setLocation(0, 0);
 		numFruitLeft = totalFruit;
 		grid[0][0] = 2;
+		
 		for(int gene = 0; gene < genes.length; gene++)
 		{
 			panel.repaint();
@@ -307,7 +343,6 @@ s	 */
 			{
 				numFruitLeft--;
 			}
-//			System.out.println("Fruitleft: " + numFruitLeft);
 			if(numFruitLeft == 0)
 			{
 				break;
@@ -316,35 +351,3 @@ s	 */
 		}
 	}
 }
-//0 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-//0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-//0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 0 0 0 0
-//0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 1 0 0
-//0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 1 0 0
-//0 0 0 1 1 1 1 0 1 1 1 1 1 0 0 0 0 0 0 0 0 1 1 0 0 0 0 0 0 0 0 0
-//0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0
-//0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-//0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0
-//0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 1 0 0
-//0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0
-//0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0
-//0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0
-//0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-//0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 1 0 0 0 0 0 1 1 1 0 0 0
-//0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 1 0 0 1 0 0 0 0 0 0 0 0
-//0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-//0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-//0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 1 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0
-//0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 1 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0
-//0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-//0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-//0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0
-//0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0
-//0 0 0 1 1 0 0 1 1 1 1 1 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-//0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-//0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-//0 1 0 0 0 0 0 0 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-//0 1 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-//0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-//0 0 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
-//0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
